69af0796f8b5afeda8f2d71f938512430b02c02f
hajer117 <h117hh@hotmail.com>
2021-12-11 16:15:25 +0300

    Completed Mandala

---
+++ Mandala-"hajerMohamed"/Mandala-"hajerMohamed"/Model/MoodEntry.swift
@@ -0,0 +1,14 @@
+//
+//  MoodEntry.swift
+//  Mandala-"hajerMohamed"
+//
+//  Created by hajer . on 04/05/1443 AH.
+//
+
+import Foundation
+
+
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
