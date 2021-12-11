0d71d7367afb9e3c3524c3651cee522f8ee53683
ABRAR ALQARNI <abraralqarni@MacBook-Pro-ABRAR.local>
2021-12-11 16:22:25 +0300

    Madala

---
+++ Mandala"ABRAR" 19/Mandala"ABRAR"/Extension/UIImage+Mandala.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+enum ImageResource: String {
+  case angry
+  case confused
+  case crying
+  case goofy
+  case happy
+  case meh
+  case sad
+  case sleepy }
+
+
+extension UIImage {
+  
+  convenience init(resource: ImageResource) {
+    self.init(named: resource.rawValue)!
+  }
+}
