e2692343fc11780281f0221c1d7e74721a94dba1
Maha S <mahas@Mahas-MacBook-Air.local>
2021-12-11 21:33:14 +0300

    Completed Mandala App

---
+++ Mandala-MahaNasser/Mandala-MahaNasser/Models/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala-MahaNasser
+//
+//  Created by Maha S on 11/12/2021.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
