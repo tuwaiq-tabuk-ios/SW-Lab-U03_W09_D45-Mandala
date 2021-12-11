28485e17af23ca90a04f957055a5513225ca2846
bUshra <91871856+bUshraNazal@users.noreply.github.com>
2021-12-11 19:21:04 +0300

    SW-Lab-U03_W09_D45-Mandala

---
+++ Mandala_Bushra-Nazal/Mandala_Bushra-Nazal/MoodsConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala_Bushra-Nazal
+//
+//  Created by bushra nazal alatwi on 05/05/1443 AH.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+  
+    func add(_ moodEntry: MoodEntry)
+  
+}
