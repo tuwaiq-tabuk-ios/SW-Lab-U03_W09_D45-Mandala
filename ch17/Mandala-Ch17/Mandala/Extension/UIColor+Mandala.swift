419a88d3ae228ae4463cd4114c82f21fe96bb3c3
macbook air <macbookair@Danah.local>
2021-12-11 16:33:38 +0300

    finish ch 17-18 - 19

---
+++ ch17/Mandala-Ch17/Mandala/Extension/UIColor+Mandala.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Mandala.swift
+//  Mandala
+//
+//  Created by macbook air on 04/05/1443 AH.
+//
+
+import UIKit
+
+extension UIColor {
+  
+  static let angry = UIColor(named: "angryRed")!
+      static let confused = UIColor(named: "confusedPurple")!
+      static let crying = UIColor(named: "cryingLightBlue")!
+      static let goofy = UIColor(named: "goofyOrange")!
+      static let happy = UIColor(named: "happyTurquoise")!
+      static let meh = UIColor(named: "mehGray")!
+      static let sad = UIColor(named: "sadBlue")!
+      static let sleepy = UIColor(named: "sleepyLightRed")!
+}
