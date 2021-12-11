df1101bdc20f068bd94cd09b5a42273967535f75
A A <aa@Areej.local>
2021-12-11 18:23:32 +0300

    Complated Mandala

---
+++ Mandala-Areej_Alanze/Mandala-Areej_Alanze/Model/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala-Areej_Alanze
+//
+//  Created by A A on 09/12/2021.
+//
+
+import Foundation
+
+struct MoodEntry {
+  var mood: Mood
+  var timestamp: Date
+}
---
+++ Mandala-Areej_Alanze/Mandala-Areej_Alanze/Model/MoodsConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala-Areej_Alanze
+//
+//  Created by A A on 11/12/2021.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+  
+  func add(_ moodEntry: MoodEntry)
+  
+}
