be0da1fc8a25aade365a92ebc66d9fb8a7b77221
Mohammed <mohammed@Mohammeds-MacBook-Air.local>
2021-12-12 14:07:19 +0300

    CH17, CH18, CH19 Completed

---
+++ CH18/Mandala-MohammedAloqbi18/Mandala-MohammedAloqbi/Model/MoodEntry.swift
@@ -0,0 +1,12 @@
+//
+//  MoodEntry.swift
+//  Mandala-MohammedAloqbi
+//
+//  Created by Mohammed on 07/05/1443 AH.
+//
+
+import UIKit
+struct MoodEntry {
+    var mood: Mood
+    var timestamp: Date
+}
---
+++ CH18/Mandala-MohammedAloqbi18/Mandala-MohammedAloqbi/Model/MoodsConfigurable.swift
@@ -0,0 +1,13 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala-MohammedAloqbi
+//
+//  Created by Mohammed on 08/05/1443 AH.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
+
