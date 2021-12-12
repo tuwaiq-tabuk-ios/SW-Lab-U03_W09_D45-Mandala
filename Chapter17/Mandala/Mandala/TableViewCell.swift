efe31f1b750fbb61e97621da8939885a2e0c07d6
Bushra alatwi <bushraalatwi@Bushras-MacBook-Air.local>
2021-12-12 08:53:07 +0300

    finish

---
+++ Chapter17/Mandala/Mandala/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Mandala
+//
+//  Created by Atheer abdullah on 04/05/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
