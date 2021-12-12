0d7c8fccf2ea0b425330182757d809612dfed8db
RAWABI BAJAFAR <91871676+RawabiBajafar@users.noreply.github.com>
2021-12-12 11:27:22 +0300

    completed

---
+++ Extensions and Container View ControllersCP18/Extensions and Container View Controllers/Extension/UIImage .swift
@@ -0,0 +1,32 @@
+//
+//  UIImage .swift
+//  Extensions and Container View Controllers
+//
+//  Created by روابي باجعفر on 05/05/1443 AH.
+//
+
+import UIKit
+
+enum ImageResource: String {
+    case angry
+    case confused
+    case crying
+    case goofy
+    case happy
+    case meh
+case sad
+case sleepy
+  
+  
+}
+
+extension UIImage {
+
+  convenience init(resource: ImageResource) {
+         self.init(named: resource.rawValue)!
+     
+  
+  
+  }
+
+}
---
+++ Extensions and Container View ControllersCP19/Extensions and Container View Controllers/Support/Extension/UIImage+Mandala.swift
@@ -0,0 +1,28 @@
+//
+//  UIImage+Mandala.swift
+//  Mandala
+//
+//  Created by macbook air on 04/05/1443 AH.
+//
+
+import UIKit
+
+enum ImageResource: String {
+  case angry
+  case confused
+  case crying
+  case goofy
+  case happy
+  case meh
+  case sad
+  case sleepy
+  
+}
+
+
+extension UIImage {
+  
+  convenience init(resource: ImageResource) {
+    self.init(named: resource.rawValue)!
+  }
+}
