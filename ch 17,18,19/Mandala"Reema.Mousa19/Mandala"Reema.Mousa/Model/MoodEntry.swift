0c9d0b7f0cddccf637d0f2e37b46237e2d73ac34
reema <reemamousa@MacBook-Pro-Reema.local>
2021-12-12 10:44:50 +0300

    Ch 17,18,19

---
+++ ch 17,18,19/Mandala"Reema.Mousa19/Mandala"Reema.Mousa/Model/MoodEntry.swift
@@ -0,0 +1,13 @@
+//
+//  MoodEntry.swift
+//  Mandala"Reema.Mousa
+//
+//  Created by Reema Mousa on 07/05/1443 AH.
+//
+
+import Foundation
+
+struct MoodEntry {
+    var mood : Mood
+    var timestamp: Date
+}
