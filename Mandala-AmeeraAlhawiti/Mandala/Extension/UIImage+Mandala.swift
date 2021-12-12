c2f6981a73d007019a83f620ee338e0a3adfc34b
Ameera BA <alqraniameera@outlook.com>
2021-12-12 09:23:41 +0300

    Finish

---
+++ Mandala-AmeeraAlhawiti/Mandala/Extension/UIImage+Mandala.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Mandala.swift
+//  Mandala
+//
+//  Created by Ameera BA on 09/12/2021.
+//
+
+import UIKit
+
+
+enum ImageResource: String {
+  case angry
+  case confused
+  case crying
+  case goofy
+  case happy
+  case meh
+  case sad
+  case sleepy
+}
+
+
+extension UIImage {
+  convenience init(resource: ImageResource) {
+    self.init(named: resource.rawValue)!
+  }
+}
